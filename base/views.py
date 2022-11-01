6d518ce7e41e1281dd10d4339c35739ccc9d9f23
naumaah <naumaan.hussain111@gmail.com>
2022-10-31 22:24:34 -0500

    Attempt

--- base/views.py
+++ base/views.py
@@ -1,30 +1,70 @@
 from ast import keyword
 from django.shortcuts import render
-from rest_framework.decorators import api_view
+from django.contrib.auth.models import User
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 
 from .models import Product
-from .serializer import ProductSerializer
+from .serializer import ProductSerializer, UserSerializer, UserSerializerWithToken
+
+from django.contrib.auth.hashers import make_password
+from rest_framework import status
+
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+from base import serializer
 
 # Create your views here.
 
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    def validate(self, attrs):
+        data = super().validate(attrs)
+
+        serializer = UserSerializerWithToken(self.user).data
+
+        for k, v in serializer.items():
+            data[k] = v
+        
+        return data
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+
+@api_view(['POST'])
+def registerUser(request):
+    data = request.data
+
+    try:
+        user = User.objects.create(
+            first_name=data['name'],
+            username=data['email'], 
+            email=data['email'],
+            password=make_password(data['password'])
+
+        )
+        serializer = UserSerializerWithToken(user, many=False)
+        return Response(serializer.data)
+    except:
+        message = {'detail':'User witht his email already exists'}
+        return Response(message, status=status.HTTP_400_BAD_REQUEST)
+
+
 @api_view(['GET'])
-def getRoutes(request):
-    routes = [
-        '/api/products/',
-        '/api/products/create/',
+@permission_classes([IsAuthenticated])
+def getUserProfile(request):
+    user = request.user
 
-        '/api/products/upload/',
+    serializer = UserSerializer(user, many=False)
+    return Response(serializer.data)
 
-        '/api/products/<id>/reviews/',
-
-        '/api/products/top/',
-        '/api/products/<id>/',
-
-        '/api/products/delete/<id>/',
-        '/api/products/<update>/<id>',
-    ]
-    return Response('Hello')
+@api_view(['GET'])
+@permission_classes([IsAdminUser])
+def getUsers(request):
+    users = User.objects.all()
+    serializer = UserSerializer(users, many=True)
+    return Response(serializer.data)
 
 @api_view(['GET'])
 def getProducts(request):
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,14 +1,26 @@
 import React from 'react'
 import SearchBox from './SearchBox'
+import { Navbar, Nav, Container, Row, NavDropdown } from 'react-bootstrap'
+import { LinkContainer } from 'react-router-bootstrap'
+
 
 function Header() {
   return (
     <div className='topnav'>
-      <a className='bold' href='/'>VUse</a>
+      <LinkContainer to='/'>
+          <Navbar.Brand>VUse</Navbar.Brand>
+      </LinkContainer>
+      {/* <a className='bold' href='/'>VUse</a> */}
       <SearchBox />
       <a href='/createlisting'>Create Listing</a>
-      <a href='/cart'>Cart</a>
-      <a href='/login'>Login</a>
+      {/* <a href='/cart'>Cart</a> */}
+      <LinkContainer to='/cart'>
+          <Nav.Link ><i className="fas fa-shopping-cart"></i>Cart</Nav.Link>
+      </LinkContainer>
+      {/* <a href='/login'>Login</a> */}
+      <LinkContainer to='/login'>
+          <Nav.Link><i className="fas fa-user"></i>Login</Nav.Link>
+      </LinkContainer>
     </div>
   )
 }
