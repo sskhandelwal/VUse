a2fb0d9cbbff5b2bb685c7acd9210de3d56cbb09
Akaash Seemakurty <akaashkasi@gmail.com>
2022-11-11 16:27:19 -0600

    Cleaned up buy now button

--- frontend/src/components/PopupComp.js
+++ frontend/src/components/PopupComp.js
@@ -4,49 +4,49 @@ import { useParams } from 'react-router-dom'
 import Modal from 'react-bootstrap/Modal';
 import { useDispatch, useSelector } from 'react-redux'
 import { updateProduct } from '../actions/productActions'
-
+ 
 function PopupComp() {
-
+ 
   const { id } = useParams()
   const productId = id
-
+ 
   const dispatch = useDispatch()
-
+ 
   const productDetails = useSelector(state => state.productDetails)
   const { product } = productDetails
-
+ 
   const userLogin = useSelector(state => state.userLogin)
   const { userInfo } = userLogin
-
+ 
   const [show, setShow] = useState(false);
   const [isBought, setBought] = useState(false);
-
+ 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
-
+ 
   const confirmHandler = (e) => {
     e.preventDefault()
     setBought(true)
     setShow(false)
-
+ 
     dispatch(updateProduct({
       _id: productId,
-      name: product.name, 
+      name: product.name,
       price: product.price,
-      description: product.description, 
-      location: product.location, 
+      description: product.description,
+      location: product.location,
       email:product.email,
       image: product.image,
       isBought,
     }))
   }
-
+ 
   return (
   <>
-  <Button variant="primary" onClick={handleShow}>
+  <Button className= "margin-left margin-right buynow" variant="primary" onClick={handleShow}>
     Buy Now
   </Button>
-
+ 
   <Modal show={show} onHide={handleClose}>
     <Modal.Header closeButton>
       <Modal.Title>Purchase Confirmation</Modal.Title>
@@ -64,5 +64,5 @@ function PopupComp() {
 </>
   )
 }
-
+ 
 export default PopupComp
